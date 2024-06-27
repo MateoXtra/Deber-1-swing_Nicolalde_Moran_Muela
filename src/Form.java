6f48350c3d7a435457cf85341cfb81b5ee3b4258
MateooMoran <brandon.moran@epn.edu.ec>
2024-06-26 20:37:14 -0500

    diseño y colores

--- src/Form.java
+++ src/Form.java
@@ -1,17 +1,17 @@
 import javax.swing.*;
 
 public class Form {
-    private JButton button1;
+    private JButton btn3;
     public JPanel panel1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
+    private JButton btn4;
+    private JButton btn2;
+    private JButton btn1;
+    private JButton btn6;
+    private JButton btn7;
+    private JButton btn8;
     private JTextField textField1;
     private JTextField textField2;
-    private JButton button9;
-    private JButton button10;
+    private JButton borrarButton;
+    private JButton btn9;
+    private JButton btn5;
 }
