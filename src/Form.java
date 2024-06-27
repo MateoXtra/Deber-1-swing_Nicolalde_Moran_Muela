7224d245c4420a2dcb4b34473aca7afed3ea448e
David <stalinsangucho87@gmail.com>
2024-06-26 20:20:21 -0500

    Interfaz

---
+++ src/Form.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Form {
+    private JButton button1;
+    private JPanel panel1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JTextField textField1;
+    private JTextField textField2;
+}
