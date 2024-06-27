8e6636abcc50861d7fce1bbe58b95c15b02e040f
MateooMoran <brandon.moran@epn.edu.ec>
2024-06-26 20:41:46 -0500

    boton multiplicacion

--- src/Form.java
+++ src/Form.java
@@ -40,5 +40,14 @@ public class Form {
 
             }
         });
+        btn3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Double numero1 = Double.parseDouble(imput1.getText());
+                Double numero2 = Double.parseDouble(imput2.getText());
+                resultado.setText(String.valueOf(numero1 * numero2));
+                resultado1.setText(null);
+            }
+        });
     }
 }
