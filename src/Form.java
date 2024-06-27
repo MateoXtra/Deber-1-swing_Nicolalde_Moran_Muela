34c92efbd582b931e22033013532325a556db5af
MateooMoran <brandon.moran@epn.edu.ec>
2024-06-26 20:40:43 -0500

    boton suma

--- src/Form.java
+++ src/Form.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Form {
     private JButton btn3;
@@ -9,9 +11,24 @@ public class Form {
     private JButton btn6;
     private JButton btn7;
     private JButton btn8;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField imput2;
+    private JTextField imput1;
     private JButton borrarButton;
     private JButton btn9;
     private JButton btn5;
+    private JLabel resultado;
+    private JLabel resultado1;
+
+
+    public Form() {
+        btn1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Double numero1 = Double.parseDouble(imput1.getText());
+                Double numero2 = Double.parseDouble(imput2.getText());
+                resultado.setText(String.valueOf(numero1 + numero2));
+                resultado1.setText(null);
+            }
+        });
+    }
 }
